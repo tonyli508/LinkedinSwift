8649cf67b244f4c2832027e85d7d0ea5ba500804
Jiantang Li <tonyli508@gmail.com>
2020-07-31 22:20:55 +0100

    try to update Linkedin

--- LinkedinSwift/LinkedinSwift/sources/LinkedinOAuthWebClient.m
+++ LinkedinSwift/LinkedinSwift/sources/LinkedinOAuthWebClient.m
@@ -8,8 +8,8 @@
 
 #import "LinkedinOAuthWebClient.h"
 
-#import <IOSLinkedInAPIFix/LIALinkedInApplication.h>
-#import <IOSLinkedInAPIFix/LIALinkedInHttpClient.h>
+#import <IOSLinkedInAPIFix1/LIALinkedInApplication.h>
+#import <IOSLinkedInAPIFix1/LIALinkedInHttpClient.h>
 
 @implementation LinkedinOAuthWebClient
 
@@ -56,7 +56,7 @@
 - (void)requestURL:(NSString* _Nonnull)url requestType:(LinkedinSwiftRequestType* _Nonnull)requestType token:(LSLinkedinToken * _Nonnull)token success:(__nullable LinkedinSwiftRequestSuccessCallback)successCallback error:(__nullable LinkedinSwiftRequestErrorCallback)errorCallback {
     
 #ifdef isSessionManager
-    [httpClient GET:url parameters:@{@"oauth2_access_token": token.accessToken} progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+    [httpClient GET:url parameters:@{@"oauth2_access_token": token.accessToken} headers:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
         successCallback([[LSResponse alloc] initWithDictionary:responseObject statusCode:200]);
     } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
         errorCallback(error);
